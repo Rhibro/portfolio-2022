069895267b819e489590b2ecccc4df7b41d34363
Rhibro <111956763+Rhibro@users.noreply.github.com>
2022-11-04 10:59:30 +0100

    added space content

--- script/script.js
+++ script/script.js
@@ -4,3 +4,17 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleBtn.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("displayImage");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 2000);
