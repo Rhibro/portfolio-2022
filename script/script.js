f61815fb6556d4d1afc40affaa0be96520ed3029
Rhibro <111956763+Rhibro@users.noreply.github.com>
2022-11-09 13:20:47 +0100

    added education section

--- script/script.js
+++ script/script.js
@@ -6,3 +6,16 @@ toggleBtn.addEventListener('click', () => {
     event.preventDefault();
 })
 
+$(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: { items: 0, nav: false },
+        600: { items: 1, nav: false },
+        1000: { items: 2, nav: false },
+    },
+  });
+
