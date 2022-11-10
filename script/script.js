a8366a15912c2c9a96f616609ddf343909a101df
Rhibro <111956763+Rhibro@users.noreply.github.com>
2022-11-10 11:00:53 +0100

    adjusting carousel

--- script/script.js
+++ script/script.js
@@ -10,12 +10,12 @@ $(".carousel").owlCarousel({
     margin: 20,
     loop: true,
     autoplay: true,
-    autoplayTimeout: 2000,
+    autoplayTimeout: 5000,
     autoplayHoverPause: true,
     responsive: {
         0: { items: 0, nav: false },
         600: { items: 1, nav: false },
-        1000: { items: 2, nav: false },
+        
     },
   });
 
